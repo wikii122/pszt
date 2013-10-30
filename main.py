0d27d7a10c090771743c276dbdc1f6d1762f2b88
Wiktor <wikii122@gmail.com>
2013-10-30 10:28:18 +0100

    Added main exec file

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- encoding:utf-8 -*-
+"""
+Program finding function minimum using evolutionary algorithm.
+
+By Andrzej Krzynówek, Wiktor Ślęczka & Radosław Więch
+"""
+from argparse import ArgumentParser
+
+DESCRIPTION = "Program finding function minimum using evolutionary algorith"
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=DESCRIPTION)
+    args = parser.parse_args() 
