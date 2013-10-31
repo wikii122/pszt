5d6be09782725faa96d9a6b2a4af3a6cbf31cec6
Wiktor <wikii122@gmail.com>
2013-10-31 15:01:10 +0100

    Stylistic changes

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ Program finding function minimum using evolutionary algorithm.
 By Andrzej Krzynówek, Wiktor Ślęczka & Radosław Więch
 """
 from argparse import ArgumentParser
-
 from simulation import Simulation
 
 DESCRIPTION = "Program finding function minimum using evolutionary algorith"
@@ -17,19 +16,19 @@ def run(valmu, lambde):
     """
     Main function of program, it's used to create and start the simulation
     """
-    print("Starting simulation with \
-           parameters mu: {0} and lambda: {1}".format(valmu, lambde))
+    print("Starting simulation with parameters \
+          mu: {0} and lambda: {1}".format(valmu, lambde))
     sim = Simulation(FUNCTION, valmu, lambde)
     sim.run()
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description=DESCRIPTION)
-    parser.add_argument("lambda", type=int,
-                        help="Number of children each parent spawns")
-    parser.add_argument("mu", type=int,
-                        help="Number of parents taken into consideration \
-                              when creating children")
+    parser.add_argument("lambda", type=int, help=\
+                        "Number of children each parent spawns")
+    parser.add_argument("mu", type=int, help=\
+                        "Number of parents taken into consideration when \
+                         creating children")
     args = parser.parse_args()
 
     run(args.mu, args.lambd)
