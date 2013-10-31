a2ce91ddfca52961be2a4a3959344882f2f4e27a
Wiktor <wikii122@gmail.com>
2013-10-31 13:08:49 +0100

    Added top level simulation handle

---
+++ simulation/simulation.py
@@ -0,0 +1,12 @@
+"""
+Top level representation of simulation
+"""
+
+class Simulation:
+    def __init__(self, func, mu=100, lamb=10):
+        self.func = func
+        self.mu = mu
+        self.lamb = lamb
+
+
+        
