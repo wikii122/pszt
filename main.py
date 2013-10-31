ab49b852a236ed270ba3d5a6ca08c7a19f016fe1
Wiktor <wikii122@gmail.com>
2013-10-31 13:09:44 +0100

    Added function to main

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ By Andrzej Krzynówek, Wiktor Ślęczka & Radosław Więch
 from argparse import ArgumentParser
 
 DESCRIPTION = "Program finding function minimum using evolutionary algorith"
+FUNCTION = lambda x1, x2: (1./3.) * x1**6 - 2.1 * x1**4 + 4 * x1**2 \
+                        + 4 * x2**4 - 4 * x2**2 + x1 * x2
 
 if __name__ == "__main__":
     parser = ArgumentParser(description=DESCRIPTION)
-    args = parser.parse_args() 
+    args = parser.parse_args()
