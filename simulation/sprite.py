1d8c0763c92e34f5894635843bd8c7c2cc7c15ad
Wiktor <wikii122@gmail.com>
2013-10-31 15:22:58 +0100

    Brough back function to sprite

--- simulation/sprite.py
+++ simulation/sprite.py
@@ -1,8 +1,12 @@
 class Sprite:
-    def __init__(self, x, y, generation=0):
+    def __init__(self, x, y, fun, generation=0):
         self.x = x
         self.y = y
+        self.fun = fun(x, y)
         self.generation = generation
 
     def spawn(self):
         pass # TODO
+
+    def value(self):
+        return self.fun
