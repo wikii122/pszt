fa8ceedbfc6080c5321d16a3be46c7930256a2db
Wiktor <wikii122@gmail.com>
2013-10-31 14:12:11 +0100

    Spawning initial generation

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -1,12 +1,26 @@
 """
 Top level representation of simulation
 """
+from . import tools
+from . import sprite
+
 
 class Simulation:
     def __init__(self, func, mu=100, lamb=10):
         self.func = func
         self.mu = mu
         self.lamb = lamb
+        self.sprites = list()
+        self.initial_spawn()
         
     def run(self):
         pass # TODO
+
+    def initial_spawn(self):
+        mu = self.mu
+        coord = tools.point_generator(seed=1, min_x=-3, min_y=-3, delta_x=6, delta_y=6)
+        while mu:
+            mu -= 1
+            sprite = sprite.Sprite(*next(coord))
+            self.sprites.append(sprite)
+
