6c761c8efaa22415b677a85423df25a7979ea1e7
Wiktor <wikii122@gmail.com>
2013-10-31 15:14:08 +0100

    Removed function from sprite

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -2,17 +2,20 @@
 Top level representation of simulation
 """
 from . import tools
-from . import sprite
+from .sprite import Sprite
 
 
 class Simulation:
+    """
+    Instance of this class represents siulated world
+    """
     def __init__(self, func, mu=100, lamb=10):
         self.func = func
         self.mu = mu
         self.lamb = lamb
         self.sprites = list()
         self.initial_spawn()
-        
+
     def run(self):
         pass # TODO
 
@@ -21,6 +24,6 @@ class Simulation:
         coord = tools.point_generator(seed=1, min_x=-3, min_y=-3, delta_x=6, delta_y=6)
         while mu:
             mu -= 1
-            sprite = sprite.Sprite(*next(coord))
+            sprite = Sprite(*next(coord))
             self.sprites.append(sprite)
 
--- simulation/sprite.py
+++ simulation/sprite.py
@@ -1,12 +1,8 @@
 class Sprite:
-    def __init__(self, x, y, fun, generation=0):
+    def __init__(self, x, y, generation=0):
         self.x = x
         self.y = y
-        self.fun = fun
         self.generation = generation
 
     def spawn(self):
         pass # TODO
-
-    def value(self):
-        return self.fun(self.x, self.y)
