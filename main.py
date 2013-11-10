71c708f8e5196c73109b39f06ecdc5d906f3352f
Wiktor <wikii122@gmail.com>
2013-11-10 09:27:37 +0100

    Removed tests as they are no more needed

--- main.py
+++ main.py
@@ -15,37 +15,17 @@ FUNCTION = lambda x1, x2: (4. * x1**2 - 2.1 * x1**4 + (1./3.) * x1**6 + \
                            x1 * x2 - 4 * x2**2 + 4 * x2**4 )
 
 
-def run(mi, lambda_, prints=True):
+def run(mi, lambda_):
     """
     Main function of program, it's used to create and start the simulation
     """
     if mi <= 1 or lambda_ <= 1:
         print("Parameters must be greater than 1")
         sys.exit(1)
-    if prints:
-        print("Starting simulation with parameters \
-               mu: {0} and lambda: {1}".format(mi, lambda_))
+    print("Starting simulation with parameters \
+           mu: {0} and lambda: {1}".format(mi, lambda_))
     sim = Simulation(FUNCTION, mi, lambda_)
-    res = sim.run(prints)
-
-    return res
-
-def test(mi, lambda_):
-    bad = []
-    trp = fap = 0.0
-    for x in range(2, args.mi):
-        for y in range(2, getattr(args, "lambda")):
-            res = run(x, y, False)
-            if res.value > -1.03:
-                fap += 1
-
-                print("Mi: {mi:>3}, lambda: {lambda_:>3} => {val}"
-                     .format(mi=x, lambda_=y, val=res.value))
-                bad.append((x, y))
-            else:
-                trp += 1
-    print(bad)
-    print("Precision: {prec}".format(prec=trp/(trp+fap)))
+    sim.run()
 
 if __name__ == "__main__":
     parser = ArgumentParser(description=DESCRIPTION)
@@ -54,13 +34,6 @@ if __name__ == "__main__":
     parser.add_argument("mi", type=int, help=\
                         "Number of parents taken into consideration when \
                          creating children")
-    #TODO Remove in final
-    parser.add_argument("--test", action="store_true", help=\
-                        "Run for every combination of parameters less \
-                         than given")
     args = parser.parse_args()
     # Getting attribute manually due to name clash.
-    if args.test:
-        test(args.mi, getattr(args, "lambda"))
-    else:
-        run(args.mi, getattr(args, "lambda"))
+    run(args.mi, getattr(args, "lambda"))
