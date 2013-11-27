19a6cb5e4b3d9f6ccaedd806350fc9b7bcbdb867
Wiktor <wikii122@gmail.com>
2013-11-27 21:04:19 +0100

    Added app window

---
+++ gui/app.py
@@ -0,0 +1,28 @@
+"""
+Main application GUI handler.
+"""
+import sys
+
+from PySide import QtGui
+
+class Application(QtGui.QApplication):
+    """
+    Main application representation.
+    """
+    def __init__(self):
+        super(Application, self).__init__(sys.argv)
+
+        self.frame = Window()
+
+class Window(QtGui.QMainWindow):
+    """
+    Main window frame.
+    """
+    def __init__(self, *args, **kwarg):
+        super(Window, self).__init__(*args, **kwarg)
+
+        self.setGeometry(100, 100, 600, 350)
+        self.setWindowTitle("E'volie")
+        self.statusBar().showMessage('Ready')
+
+        self.show()
