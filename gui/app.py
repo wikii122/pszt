afbea108f4eebb6f367036d229dae1bf8c1245c1
Wiktor <wikii122@gmail.com>
2013-11-28 11:52:14 +0100

    Added Edit Widget

--- gui/app.py
+++ gui/app.py
@@ -27,8 +27,29 @@ class Window(QtGui.QMainWindow):
     def __init__(self, *args, **kwarg):
         super(Window, self).__init__(*args, **kwarg)
 
+        widget = QtGui.QWidget()
+
         self.setGeometry(100, 100, 600, 350)
-        self.setWindowTitle("E'volie")
+        self.setCentralWidget(widget)
+        self.setWindowTitle("E'voile")
         self.statusBar().showMessage('Ready')
 
         self.show()
+
+    class EditWidget(QtGui.QWidget):
+        """
+        Widget used for handling input.
+        """
+        def __init__(self, *args, **kwarg):
+            super(Window.EditWidget, self).__init__(*args, **kwarg)
+
+            self.layout = QtGui.QFormLayout()
+
+        def set_labels(self, labels):
+            """
+            Set label names in widget.
+            """
+            for label in labels:
+                edit = QtGui.QLineEdit()
+                self.layout.addRow(QtGui.QLabel(label), edit)
+
