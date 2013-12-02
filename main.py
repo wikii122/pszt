e4f3818ad8b58ea0e3f61462d82a3c111cafa9a5
Wiktor <wikii122@gmail.com>
2013-12-02 18:32:59 +0100

    Integrated GUI with main

--- main.py
+++ main.py
@@ -14,32 +14,43 @@ from argparse import ArgumentParser
 path = os.path.dirname(sys.argv[0])
 path = os.path.abspath(path)
 sys.path.append(path)
+
 from simulation import Simulation
+from gui.app import Application
 
 DESCRIPTION = "Program finding function minimum using evolutionary algorithm"
 FUNCTION = lambda x1, x2: (4. * x1**2 - 2.1 * x1**4 + (1./3.) * x1**6 + \
                            x1 * x2 - 4 * x2**2 + 4 * x2**4 )
 
-
-def run(mi, lambda_):
+def run():
     """
     Main function of program, it's used to create and start the simulation
     """
-    if mi <= 1 or lambda_ <= 1:
-        print("Parameters must be greater than 1")
-        sys.exit(1)
-    print("Starting simulation with parameters \
-           mu: {0} and lambda: {1}".format(mi, lambda_))
-    sim = Simulation(FUNCTION, mi, lambda_)
-    sim.run()
+    sim = Simulation(FUNCTION)
+    app = Application()
+    # TODO
+    #app.set_simulation(sim)
+    app.run()
 
+#def run(mi, lambda_):
+#    """
+#    Main function of program, it's used to create and start the simulation
+#    """
+#    if mi <= 1 or lambda_ <= 1:
+#        print("Parameters must be greater than 1")
+#        sys.exit(1)
+#    print("Starting simulation with parameters \
+#           mu: {0} and lambda: {1}".format(mi, lambda_))
+#    sim = Simulation(FUNCTION, mi, lambda_)
+#    sim.run()
+#
 if __name__ == "__main__":
     parser = ArgumentParser(description=DESCRIPTION)
-    parser.add_argument("mi", type=int, help=\
-                        "Number of parents taken into consideration when \
-                         creating children")
-    parser.add_argument("lambda", type=int, help=\
-                        "Number of children each parent spawns")
+#    parser.add_argument("mi", type=int, help=\
+#                        "Number of parents taken into consideration when \
+#                         creating children")
+#    parser.add_argument("lambda", type=int, help=\
+#                        "Number of children each parent spawns")
     parser.add_argument("--debug", action="store_true", help=\
                         "Use fixed seed for random generator")
 
@@ -47,4 +58,5 @@ if __name__ == "__main__":
     if args.debug:
         random.seed(1)
     # Getting attribute manually due to name clash.
-    run(args.mi, getattr(args, "lambda"))
+    #run(args.mi, getattr(args, "lambda"))
+    run()
