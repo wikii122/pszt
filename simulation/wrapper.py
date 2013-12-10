1423c0c6a3cc3044243f761c31846ead39a5722f
Wiktor <wikii122@gmail.com>
2013-12-10 15:16:45 +0100

    Added interface for simulation wrapper

--- simulation/wrapper.py
+++ simulation/wrapper.py
@@ -3,11 +3,33 @@ Wrapper for simulation class.
 """
 
 from PySide.QtCore import QObject
+from simulation.simulation import Simulation
 
 class SimulationWrapper(QObject):
     """
-    Control interface for simulation.
+    Control interface for simulation and separate thread used for running it.
     """
     def __init__(self):
         super(SimulationWrapper, self).__init__()
+        self.simulation = None
 
+    def start(self):
+        """
+        Function used to handle the signal of starting the simulation.
+        """
+        self.simulation = Simulation() # TODO missing arguments
+
+    def pause(self):
+        """
+        Function used to handle the signal of stopping the simulation.
+        """
+        pass
+
+    def continue_(self):
+        """
+        Function used to handle the signal of continuation of the simulation.
+        """
+        pass
+
+    def _thread_run(self):
+        pass
