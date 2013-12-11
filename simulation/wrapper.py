e6595b8cbf3a987dcb2e402d17088c65ab43874b
Wiktor <wikii122@gmail.com>
2013-12-11 23:05:59 +0100

    Converted methods to slots

--- simulation/wrapper.py
+++ simulation/wrapper.py
@@ -2,32 +2,48 @@
 Wrapper for simulation class.
 """
 
-from PySide.QtCore import QThread
+from PySide.QtCore import QThread, Slot, Signal
 from simulation.simulation import Simulation
 
 class SimulationWrapper(QThread):
     """
     Control interface for simulation and separate thread used for running it.
     """
+
+    graph_changed = Signal(object)  # TODO type needs to be precised
+
     def __init__(self, parent=None):
         super(SimulationWrapper, self).__init__(parent)
         self.simulation = None
         self.running = False
 
     def start(self):
+        self.start_()
+        super(SimulationWrapper, self).start()
+
+    def update_graph(self):
+        """
+        Function used to generate new graph and send appropiate signal to
+        window.
+        """
+        pass  # TODO: make this
+
+    @Slot()
+    def start_(self):
         """
         Function used to handle the signal of starting the simulation.
         """
         self.simulation = Simulation() # TODO missing arguments
         self.running = True
-        super(SimulationWrapper, self).start()
 
+    @Slot()
     def pause(self):
         """
         Function used to handle the signal of stopping the simulation.
         """
         self.running = True
 
+    @Slot()
     def continue_(self):
         """
         Function used to handle the signal of continuation of the simulation.
