7653c0fad92997122e578b2c85418c763b27a007
Wiktor <wikii122@gmail.com>
2014-01-09 19:26:41 +0100

    Changed imports in unified fashion

--- gui/graphwidget.py
+++ gui/graphwidget.py
@@ -1,12 +1,10 @@
 """
 GUI element responsible for drawing and displaying graph.
 """
-from PySide import QtGui, QtCore, QtWebKit
+import sys
 import pygal
 
-import sys
-from PySide.QtCore import QByteArray
-from PySide.QtWebKit import QWebView
+from PySide import QtGui, QtCore, QtWebKit
 
 
 class GraphWidget(QtGui.QWidget):
@@ -14,15 +12,15 @@ class GraphWidget(QtGui.QWidget):
     def __init__(self, sim, parent=None):
         super(GraphWidget, self).__init__(parent)
         sim.graph_changed.connect(self.showgraph)
-        self.web = QWebView()
+        self.web = QtWebKit.QWebView()
         self.layout = QtGui.QVBoxLayout();
         self.initView()
 
 
     @QtCore.Slot()
     def showgraph(self, data):
-        Qdata = QByteArray(data)
-        self.web.setContent(Qdata)
+        qdata = QtCore.QByteArray(data)
+        self.web.setContent(qdata)
         self.layout.addWidget(self.web)
         self.setLayout(self.layout)
 
@@ -30,7 +28,7 @@ class GraphWidget(QtGui.QWidget):
         self.xy_chart = pygal.XY(stroke=False, show_legend = False, no_data_text = '', title_font_size = 25)
         self.xy_chart.title = ("Projekt PSZT- Poszukiwanie Miniumum Funkcji")
         TextData = self.xy_chart.render()
-        Qdata = QByteArray(TextData)
-        self.web.setContent(Qdata)
+        qdata = QtCore.QByteArray(TextData)
+        self.web.setContent(qdata)
         self.layout.addWidget(self.web)
         self.setLayout(self.layout)
